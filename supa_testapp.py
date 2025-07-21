105fbd31bc99b3809127c6a412b4261500355038
StefDo-lab <stefan.dorfstetter1@gmail.com>
2025-07-21 10:28:48 +0200

    test erweiterung

--- supa_testapp.py
+++ supa_testapp.py
@@ -9,4 +9,21 @@ import json
 from supabase import create_client, Client
 
 st.write("Imports funktionieren!")
+# ---- Configuration ----
+SUPABASE_URL = st.secrets["supabase_url"]
+SUPABASE_KEY = st.secrets["supabase_service_role_key"]
+TABLE_WORKOUT = "workouts"
+TABLE_ARCHIVE = "workout_history"
+TABLE_QUESTIONNAIRE = "questionaire"
+
+HEADERS = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}",
+    "Content-Type": "application/json"
+}
+
+# Supabase Client für Auth
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+st.write("Configuration geladen!")
 

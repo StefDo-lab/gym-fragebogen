fc7c384dbbf0ebddee8ec0a3e93c11f0cce61451
StefDo-lab <stefan.dorfstetter1@gmail.com>
2025-07-21 10:27:43 +0200

    tst2

--- supa_testapp.py
+++ supa_testapp.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write("Test")
+import datetime
+import requests
+import pandas as pd
+import re
+from openai import OpenAI
+import io
+import json
+from supabase import create_client, Client
+
+st.write("Imports funktionieren!")
+
